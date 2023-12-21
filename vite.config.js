9cbf1193181e00181c76f3c9e4f8f146dc6b0e01
Niiranen Oska <oska.niiranen@gmail.com>
2023-12-21 11:52:04 +0200

    2.12

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/07-tehtavat-2.11",
+  base: "/08-tehtavat-2.12-2.15",
   plugins: [react()],
 })
